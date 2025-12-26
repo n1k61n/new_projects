e90f18a385251214af5af2f67c49fbbb626495a3
Lkhan05 <m.elxan2005@gmail.com>
2025-12-26 17:19:12 +0400

    test

--- frutable/src/main/java/com/example/fruitables/config/AppConfig.java
+++ frutable/src/main/java/com/example/fruitables/config/AppConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class AppConfig  {
